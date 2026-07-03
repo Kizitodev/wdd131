ad9fa32f35a5a9ad9f4438072b2c0f66e754c9b5
kizitodev <kizitobenedict8@gmail.com>
2026-07-03 11:43:46 +0100

    Initial commit

---
+++ scripts/getdate.js
@@ -0,0 +1,12 @@
+// Select the DOM elements for output
+const currentYearElement = document.getElementById('current-year');
+const lastModifiedElement = document.getElementById('last-modified');
+
+// Get the current date and last modified date
+const currentYear = new Date();
+const lastModified = document.lastModified;
+
+// Update the DOM elements with the dates
+currentYearElement.textContent = currentYear.getFullYear();
+lastModifiedElement.textContent = lastModified;
+
