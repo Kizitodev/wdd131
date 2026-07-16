feb6b7e0921754664e0ffc2cc868c962739551a8
kizitodev <kizitobenedict8@gmail.com>
2026-07-16 21:34:05 +0100

    updated img

--- scripts/place.js
+++ scripts/place.js
@@ -15,12 +15,7 @@ const temperature = 25;
 const windSpeed = 10;
 
 function calculateWindChill(temp, speed) {
-    return (
-        13.12 +
-        (0.6215 * temp) -
-        (11.37 * Math.pow(speed, 0.16)) +
-        (0.3965 * temp * Math.pow(speed, 0.16))
-    ).toFixed(1);
+    return ( 13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) +(0.3965 * temp * Math.pow(speed, 0.16))  ).toFixed(1);
 }
 
 const windChill = document.getElementById("windchill");
