c39348c01fd66e831f3bf9096d5e7df86f3345c0
kizitodev <kizitobenedict8@gmail.com>
2026-07-25 18:58:25 +0100

    updated h2

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -113,7 +113,7 @@ const temples = [
     templeFilter.forEach((temple) => {
 
         let card = document.createElement("section");
-        let name = document.createElement("h3");
+        let name = document.createElement("h2");
         let location = document.createElement("p");
         let dedicated = document.createElement("p");
         let area = document.createElement("p");
